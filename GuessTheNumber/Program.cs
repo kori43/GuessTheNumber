68ed1e73ca4444045f962638038d8951a3def08f
Richard <94477473+kori43@users.noreply.github.com>
2025-01-03 02:40:20 +0500

    the game is now in a loop

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,30 +1,42 @@
 ﻿Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
-Console.WriteLine("Попробуй угадать число от 1 до 100.");
-
-Random random = new Random();
-int randNum = random.Next(1, 101);
-int userNum = 0;
-int attempts  = 0;
-
-while (userNum != randNum)
+bool play = true;
+while (play)
 {
-    Console.Write("Введите ваше число: ");
-    string input = Console.ReadLine();
+    Console.WriteLine("\nВведите 'quit' чтобы выйти.");
+    Console.WriteLine("Попробуй угадать число от 1 до 100.");
+    
+    Random random = new Random();
+    int randNum = random.Next(1, 101);
+    int userNum = 0;
+    int attempts = 0;
 
-    attempts++;
-    if(int.TryParse(input, out userNum))
+    while (randNum != userNum)
     {
-        if (userNum < randNum)
-            Console.WriteLine("Загаданное число больше.");
+        Console.Write("Введите ваше число: ");
+        string input = Console.ReadLine();
 
-        else if (userNum > randNum)
-            Console.WriteLine("Загаданное число меньше.");
+        if (input == "quit")
+        {
+            play = false;
+            break;
+        }           
         else
-            Console.WriteLine($"Поздравляю! Ты угадал число {randNum}!");
-    }
-    else
-        Console.WriteLine("Пожалуйста, введите корректное число.");   
+        {            
+            if (int.TryParse(input, out userNum))
+            {
+                attempts++;
+                if (userNum < randNum)
+                    Console.WriteLine("Загаданное число больше.");                
+                else if (userNum > randNum)
+                    Console.WriteLine("Загаданное число меньше.");            
+                else
+                {
+                    Console.WriteLine($"\nПоздравляю! Ты угадал число {randNum}!\nВаши попытки {attempts}.");
+                    break;
+                }               
+            }
+            else
+                Console.WriteLine("Пожалуйста, введите корректное число.");
+        }
+    } 
 }
-
-Console.WriteLine($"\nСпасибо за игру! Ваши попытки {attempts}. \nНажмите любую клавишу, чтобы выйти.");
-Console.ReadKey();
